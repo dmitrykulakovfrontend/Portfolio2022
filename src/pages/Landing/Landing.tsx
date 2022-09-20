9035a629e660ec9118435d1694b237663d7f7526
Dmitry Kulakov <atomeistee@gmail.com>
2022-09-20 16:13:09 +0300

    initial styling

---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Landing.scss";
+
+const Landing = () => {
+  return (
+    <div className="landing-page">
+      <h1>Landing</h1>
+    </div>
+  );
+};
+
+export default Landing;
