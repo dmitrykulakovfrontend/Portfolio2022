7350f70c17745752d27d71a68f14a0d206318321
Dmitry Kulakov <atomeistee@gmail.com>
2022-09-20 16:10:33 +0300

    create useWindowSize

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,4 +1,6 @@
+import useWindowSize from "hooks/useWindowSize";
 import variables from "utils/variables";
+  const { width } = useWindowSize();
           width && width < variables["screen-xl"]
             ? "page-menu closed"
             : "page-menu active"
