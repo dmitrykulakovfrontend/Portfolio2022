8c609f50591bbcc7078384e3d683e888cb6620a0
Dmitry Kulakov <atomeistee@gmail.com>
2022-10-15 16:40:58 +0300

    feat: update socials links

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,38 +1,67 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Socials.scss";
 const Socials = () => {
   return (
     <ul className="socials-list">
       <li className="socials-list__list-item">
-        <Link to="" className="socials-list__link">
+        <a
+          href="https://github.com/dmitrykulakovfrontend"
+          target="_blank"
+          className="socials-list__link"
+          rel="noreferrer"
+        >
           <i className="uil uil-github socials-list__icon"></i>
-        </Link>
+        </a>
       </li>
       <li className="socials-list__list-item">
-        <Link to="" className="socials-list__link">
+        <a
+          href="https://www.linkedin.com/in/dmitrykulakovfrontend/"
+          target="_blank"
+          className="socials-list__link"
+          rel="noreferrer"
+        >
           <i className="uil uil-linkedin socials-list__icon"></i>
-        </Link>
+        </a>
       </li>
       <li className="socials-list__list-item">
-        <Link to="" className="socials-list__link">
+        <a
+          href="https://twitter.com/AtomEistee"
+          target="_blank"
+          className="socials-list__link"
+          rel="noreferrer"
+        >
           <i className="uil uil-twitter socials-list__icon"></i>
-        </Link>
+        </a>
       </li>
       <li className="socials-list__list-item">
-        <Link to="" className="socials-list__link">
+        <a
+          href="https://telegram.me/dmitrykulakovfrontend"
+          target="_blank"
+          className="socials-list__link"
+          rel="noreferrer"
+        >
           <i className="uil uil-telegram socials-list__icon"></i>
-        </Link>
+        </a>
       </li>
       <li className="socials-list__list-item">
-        <Link to="" className="socials-list__link">
+        <a
+          href="https://wa.me/79855887126"
+          target="_blank"
+          className="socials-list__link"
+          rel="noreferrer"
+        >
           <i className="uil uil-whatsapp socials-list__icon"></i>
-        </Link>
+        </a>
       </li>
       <li className="socials-list__list-item">
-        <Link to="" className="socials-list__link">
+        <a
+          href="https://vk.com/atomeistee"
+          target="_blank"
+          className="socials-list__link"
+          rel="noreferrer"
+        >
           <i className="uil uil-vk socials-list__icon"></i>
-        </Link>
+        </a>
       </li>
     </ul>
   );
