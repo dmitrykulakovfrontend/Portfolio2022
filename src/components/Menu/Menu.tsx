0f898ba7fbe7b762f873f6279dd401adee7530de
Dmitry Kulakov <atomeistee@gmail.com>
2022-10-15 16:42:38 +0300

    style: finish menu styling

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import "./Menu.scss";
 
 import { Outlet } from "react-router-dom";
+import useScrollspy from "hooks/useScrollspy";
 
 const Menu = () => {
   const [isActive, setIsActive] = useState(false);
+  const activeSection = useScrollspy(["home", "about"]);
 
   const handleClick = () => {
     setIsActive(!isActive);
@@ -21,7 +23,90 @@ const Menu = () => {
           }
         ></i>
       </button>
-      <ul className={isActive ? "menu menu_active" : "menu"}></ul>
+      <header className={isActive ? "menu menu_active" : "menu"}>
+        <nav className="menu__nav">
+          <ul className="menu__list">
+            <li className="menu__list-item">
+              <a
+                href="#home"
+                className={
+                  activeSection === "home"
+                    ? "menu__link menu__link_active"
+                    : "menu__link"
+                }
+              >
+                <i className="menu__icon uil uil-home"></i>
+                Home
+              </a>
+            </li>
+            <li className="menu__list-item">
+              <a
+                href="#about"
+                className={
+                  activeSection === "about"
+                    ? "menu__link menu__link_active"
+                    : "menu__link"
+                }
+              >
+                <i className="menu__icon uil uil-user"></i>
+                About
+              </a>
+            </li>
+            <li className="menu__list-item">
+              <a
+                href="#resume"
+                className={
+                  activeSection === "resume"
+                    ? "menu__link menu__link_active"
+                    : "menu__link"
+                }
+              >
+                <i className="menu__icon uil uil-file"></i>
+                Resume
+              </a>
+            </li>
+            <li className="menu__list-item">
+              <a
+                href="#portfolio"
+                className={
+                  activeSection === "portfolio"
+                    ? "menu__link menu__link_active"
+                    : "menu__link"
+                }
+              >
+                <i className="menu__icon uil uil-bag"></i>
+                Portfolio
+              </a>
+            </li>
+            <li className="menu__list-item">
+              <a
+                href="#services"
+                className={
+                  activeSection === "services"
+                    ? "menu__link menu__link_active"
+                    : "menu__link"
+                }
+              >
+                <i className="menu__icon uil uil-server"></i>
+                Services
+              </a>
+            </li>
+            <li className="menu__list-item">
+              <a
+                href="#contact"
+                className={
+                  activeSection === "contact"
+                    ? "menu__link menu__link_active"
+                    : "menu__link"
+                }
+              >
+                <i className="menu__icon uil uil-envelope"></i>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 };
