d160bcac43ad4be9ccedd37dea87df246c3eb6e4
Dmitry Kulakov <atomeistee@gmail.com>
2022-10-31 15:48:30 +0300

    feat: Google Analytics 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-console */
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "pages/Landing";
 import Menu from "components/Menu";
 import "./App.scss";
+import GA4React from "ga-4-react";
 import useScrollspy from "hooks/useScrollspy";
 
 function App() {
@@ -19,6 +21,15 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
+}
+
+try {
+  setTimeout(() => {
+    const ga4react = new GA4React(process.env.REACT_APP_GA4!);
+    ga4react.initialize().catch((err) => console.error(err));
+  }, 4000);
+} catch (err) {
+  console.error(err);
 }
 
 export default App;
