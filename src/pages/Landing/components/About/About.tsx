647e79ce649cc1004a5dd4af79276ebe503e68ed
Dmitry Kulakov <atomeistee@gmail.com>
2022-11-19 15:15:42 +0300

    fix: path to new image

--- src/pages/Landing/components/About/About.tsx
+++ src/pages/Landing/components/About/About.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Section from "../Section";
 import "./About.scss";
 
-import test2 from "static/test2.webp";
+import profileImg from "static/profile.webp";
 import InfoList from "./components/InfoList";
 
 const profile = {
@@ -18,7 +18,7 @@ const About = () => {
   return (
     <Section title="About" info="Here you can learn about me">
       <div className="about">
-        <img className="about__profile" src={test2} alt="profile" />
+        <img className="about__profile" src={profileImg} alt="profile" />
         <div className="about__text">
           <h3 className="about__title">Front End Developer.</h3>
           <p className="about__info">
@@ -31,8 +31,7 @@ const About = () => {
             stack MERN application. Self-taught web developer. 1 year of Web
             Development and JavaScript/React. Used GitHub Flow during working
             with other developers. Currently Looking for a remote job with
-            friendly and communicative team. Scrum/Agile methodologies in
-            priority.
+            friendly and communicative team.
           </p>
         </div>
       </div>
