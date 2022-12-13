bdf312ef436939efafe60071674190f8a8f716e9
Dmitry Kulakov <atomeistee@gmail.com>
2022-12-13 10:03:18 +0300

    Updated about

--- src/pages/Landing/components/About/About.tsx
+++ src/pages/Landing/components/About/About.tsx
@@ -7,12 +7,21 @@ import InfoList from "./components/InfoList";
 
 const profile = {
   hobbies: "Books, video-games",
-  goal: "Become a Senior Developer",
+  goal: "Develop products with great user experience",
   city: "Moscow, Russia",
   email: "atomeistee@gmail.com",
-  experience: "1 year of Web Development",
+  technologies: "TanStack, tRPC, TypeScript, Tailwind",
   freelance: "Available",
 };
+
+const bulletPoints = [
+  "Self-taught front-end software engineer with great experience in web development and React.",
+  "Worked with: MongoDB, Express, React, Node.js, TanStack products, tRPC, Prisma, Typescript, and Next.",
+  "Collaborated with many other web developers in eager to create a successfull applications.",
+  "Have experience with GitHub flow and git.",
+  "Looking for a remote job with a team that is really love what they are doing, and ready to share their excitement.",
+  "Check out my portfolio page and projects, including Books Hut, Paladins Data, and Todo Daily.",
+];
 
 const About = () => {
   return (
@@ -20,18 +29,49 @@ const About = () => {
       <div className="about">
         <img className="about__profile" src={profileImg} alt="profile" />
         <div className="about__text">
-          <h3 className="about__title">Front End Developer.</h3>
+          <h3 className="about__welcome">Hi, I&apos;m Dmitry!</h3>
+          <h4 className="about__title">Front End Developer.</h4>
           <p className="about__info">
             I am a Web Developer from Russia with a good experience in
             collaboration with developers and people.
           </p>
           <InfoList profile={profile} />
+          <ul className="about__bullet-list">
+            {bulletPoints.map((bullet, i) => (
+              <li key={i} className="about__bullet">
+                {bullet}
+              </li>
+            ))}
+          </ul>
           <p className="about__description">
-            Collaborated with other web developers in projects. Created full
-            stack MERN application. Self-taught web developer. 1 year of Web
-            Development and JavaScript/React. Used GitHub Flow during working
-            with other developers. Currently Looking for a remote job with
-            friendly and communicative team.
+            I am a self-taught front-end software engineer based in Moscow,
+            Russia. I have good experience in web development and have used many
+            different technologies in projects where I developed, such as:
+            MongoDB, Express, React, Node.js, TanStack products, tRPC, Prisma,
+            Typescript, and Next. I have honed these skills through self-study,
+            collaboration with other developers, and hands-on experience
+            creating full-stack applications. I am proficient in using GitHub
+            Flow to work efficiently with a team.
+          </p>
+          <p className="about__description">
+            I am currently looking for a remote job that allows me to work with
+            a team that really enjoy web development. I am a proactive and
+            reliable team member who is dedicated to delivering high-quality
+            results.
+          </p>
+          <p className="about__description">
+            In addition to my experience and skills, I have a portfolio of
+            projects that showcases my capabilities as a front-end software
+            engineer. These projects include a website library with the ability
+            to create an account and access a wide range of books, a web
+            application that uses Hi-Rez Studios API to show user statistics
+            from the game called Paladins, and a CRUD task-oriented application
+            that stores data in LocalStorage.
+          </p>
+          <p className="about__description">
+            Thank you for considering me for your web development needs. I am
+            eager to put my skills to use in a new role and contribute to the
+            success of your team.
           </p>
         </div>
       </div>
